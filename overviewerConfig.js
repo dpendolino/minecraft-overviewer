9e4192328a6cf9b92bc23fe9ec24d4c8d34d4378
Daniel Pendolino <dpendolino@gmail.com>
2022-12-21 10:44:49 -0500

    Wed Dec 21 10:44:49 EST 2022

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1671495744",
+        "cacheTag": "1671636535",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1671486538,
+            "last_rendertime": 1671635206,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
